701be16d84f29e7a38f65406daccc574e1c060d7
Trey Owen <treyeowen@gmail.com>
2024-06-08 20:27:36 -0500

    Add math engine

---
+++ js/math_engine.js
@@ -0,0 +1,14 @@
+
+export default class MathEngine {
+  constructor() {
+    this.operations = {};
+    this.metadata = {
+      name,
+      inputType
+    };
+  }
+
+  function getOperations() {
+    return this.operations;
+  }
+}
